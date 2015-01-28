e62a19a0c33653990336cec58c7cdf134a294f3a
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2015-01-28 08:56:27 +0100

    Let composer handle autoloading

--- config/application.config.php
+++ config/application.config.php
@@ -9,10 +9,6 @@ return [
     ],
 
     'module_listener_options' => [
-        'module_paths' => [
-            './module',
-        ],
-
         'config_glob_paths' => [
             'config/autoload/{,*.}{global,local}.php',
         ],
